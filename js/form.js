482191835115147ccee3b7eb9ead9485a4e574da
STom28 <tomisauerpro@gmail.com>
2022-09-27 14:09:31 -0300

    add form

---
+++ js/form.js
@@ -0,0 +1,12 @@
+const tarjeta = document.querySelector('#tarjeta'),
+    btnAbrirFormulario = document.querySelector('#btn-abrir-formulario'),
+    formulario = document.querySelector('#formulario-tarjeta');
+
+tarjeta.addEventListener('click',()=>{
+    tarjeta.classList.toggle('active');
+});
+
+btnAbrirFormulario.addEventListener('click', ()=>{
+    btnAbrirFormulario.classList.toggle('active');
+    formulario.classList.toggle('active');
+});
